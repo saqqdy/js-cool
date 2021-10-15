ed146d46a6e356db1dcdff9ecc0f60c26c299380
saqqdy <saqqdy@qq.com>
2021-10-15 13:56:43 +0800

    新增 contains/unique 方法

---
+++ src/contains.ts
@@ -0,0 +1,20 @@
+/**
+ * 数组是否包含指定元素
+ *
+ * @example
+ * ```js
+ * contains([1, 2], 2) // true
+ * contains([1, 2], 3) // false
+ * ```
+ * @param arr - 目标数组
+ * @param item - 要查找的目标
+ * @returns boolean
+ */
+function contains(arr: any[], item: any): boolean {
+    for (const el of arr) {
+        if (el === item) return true
+    }
+    return false
+}
+
+export default contains
---
+++ src/unique.ts
@@ -0,0 +1,19 @@
+import contains from './contains'
+/**
+ * 数组去重
+ *
+ * @example
+ * ```js
+ * unique([1, 2, 2, '33']) // [1, 2, '33']
+ * ```
+ * @returns array
+ */
+function unique<T = unknown>(arr: T[]): T[] {
+    let newArray: T[] = []
+    for (const el of arr) {
+        !contains(newArray, el) && newArray.push(el)
+    }
+    return newArray
+}
+
+export default unique
