d2bd9ce9bf33057ffbe0d766eb8b3ee9a7707281
saqqdy <saqqdy@users.noreply.github.com>
2023-03-24 14:10:39 +0800

    script

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,5 +1,3 @@
 import { execSync } from 'child_process'
 
-const cmd = process.platform === 'win32' ? 'start' : 'open'
-
-execSync(`${cmd} https://npmmirror.com/sync/js-cool`)
+execSync('curl -X PUT -d "sync_upstream=true" "https://registry-direct.npmmirror.com/js-cool/sync"')
