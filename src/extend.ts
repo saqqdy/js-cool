d91e9372dc96793f064bc1b5b4b0569325c8760a
saqqdy <saqqdy@users.noreply.github.com>
2023-03-31 18:28:14 +0800

    new extend

--- src/extend.ts
+++ src/extend.ts
@@ -1,6 +1,11 @@
 import type { ArrayOneMore } from '../typings/common'
 import isArray from './isArray'
-import getType from './getType'
+
+export type ExtendArrayData = unknown[]
+
+export type ExtendObjectData = Record<string, unknown>
+
+export type ExtendData = ExtendArrayData | ExtendObjectData
 
 function isWindow(obj: any) {
 	return obj !== null && obj === obj.window
@@ -8,11 +13,39 @@ function isWindow(obj: any) {
 
 function isPlainObject(obj: any) {
 	return (
-		getType(obj) === 'object' &&
+		Object.prototype.toString.call(obj) === '[object Object]' &&
 		!isWindow(obj) &&
 		Object.getPrototypeOf(obj) === Object.prototype
 	)
 }
+
+// function extend(target: ExtendArrayData, source: ExtendArrayData): ExtendArrayData
+// function extend(target: ExtendObjectData, source: ExtendObjectData): ExtendObjectData
+// function extend(deep: boolean, target: ExtendArrayData, source: ExtendArrayData): ExtendArrayData
+// function extend(deep: boolean, target: ExtendObjectData, source: ExtendObjectData): ExtendObjectData
+// // function extend(target: boolean | ExtendData, ...args: ArrayOneMore<ExtendData>): ExtendData
+// function extend(target: boolean | ExtendData, ...args: ArrayOneMore<ExtendData>): ExtendData {
+// 	let deep = false
+// 	if (typeof target === 'boolean') {
+// 		deep = target
+// 		target = args.shift() as ExtendData
+// 	}
+// 	console.log(100, deep, target, args)
+// 	args.forEach(source => {
+// 		for (const key in source)
+// 			if (source.hasOwnProperty(key)) {
+// 				if (deep && (isPlainObject(source[key]) || isArray(source[key]))) {
+// 					if (isPlainObject(source[key]) && !isPlainObject(target[key])) target[key] = {}
+// 					if (isArray(source[key]) && !isArray(target[key])) target[key] = []
+// 					extend(deep, target[key], source[key])
+// 				} else if (source[key] !== undefined) target[key] = source[key]
+// 			}
+// 		// extend(deep, target, source)
+// 	})
+// 	return target
+// }
+
+// console.log(extend(true, {}, { a: 11 }, { b: 22 }))
 
 // 对象扩展
 const extend = (function () {
