5430bc23dba00cd720f80f0b3b2f5b0daa443e4d
saqqdy <saqqdy@qq.com>
2025-01-17 10:30:19 +0800

    release 5.23.1

--- typedoc.js
+++ typedoc.js
@@ -1,12 +1,35 @@
-module.exports = {
+/** @type {Partial<import("typedoc").TypeDocOptions>} */
+const config = {
 	out: 'docs',
-	entryPoints: ['src/index.ts'],
+	entryPoints: ['./src'],
 	json: 'docs/out.json',
-	name: 'index.md',
+	name: 'JS-COOL DOCS',
 	includeVersion: true,
-	readme: 'README.md'
+	readme: 'README.md',
+	sort: ['source-order'],
+	categorizeByGroup: false,
+	searchCategoryBoosts: {
+		Component: 2,
+		Model: 1.2
+	},
+	searchGroupBoosts: {
+		Classes: 1.5
+	},
+	hostedBaseUrl: 'https://www.saqqdy.com/js-cool/',
+	navigationLinks: {
+		Docs: 'https://www.saqqdy.com/js-cool/',
+		GitHub: 'https://github.com/saqqdy/js-cool',
+		CHANGELOG: 'https://github.com/saqqdy/js-cool/blob/master/CHANGELOG.md'
+	},
+	// highlightLanguages: ['typescript', 'tsx', 'css', 'json', 'jsonc', 'python', 'yaml', 'markdown'],
+	markdownItOptions: {
+		html: true
+	},
+	suppressCommentWarningsInDeclarationFiles: true
 	// emit: true,
 	// exclude: '',
 	// externalPattern: '',
 	// excludeExternals: ''
 }
+
+module.exports = config
